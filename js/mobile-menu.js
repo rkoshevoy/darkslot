0fb97f5f625e71ce8ba14c8345bd94dce697bf12
Ruslan Koshevoy <ruslan.koshevoj@gmail.com>
2017-06-26 16:54:34 +0300

    blog & some fixes

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -8,4 +8,16 @@ $(document).ready(function() {
 		$('.main-nav__list').removeClass('main-nav__list--show')
 	});
 
+	$('.main-nav__item--first').click(function() {
+		$('.main-nav__dropdown--first').toggleClass('hidden')
+	});
+
+	$('.main-nav__item--second').click(function() {
+		$('.main-nav__dropdown--second').toggleClass('hidden')
+	});
+
+	$('.main-nav__item--fifth').click(function() {
+		$('.main-nav__dropdown--fifth').toggleClass('hidden')
+	});
+
 });
