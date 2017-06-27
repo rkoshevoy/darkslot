4fb8fab083afcbd006263c4daef08f373778f767
Ruslan Koshevoy <ruslan.koshevoj@gmail.com>
2017-06-27 10:23:12 +0300

    some fixes

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,20 +4,29 @@ $(document).ready(function() {
 		$('.main-nav__list').toggleClass('main-nav__list--show')
 	});
 
-	$('.main-nav__item').click(function() {
+	$('.main-nav__item--hider').click(function() {
 		$('.main-nav__list').removeClass('main-nav__list--show')
 	});
 
+	$('.main-nav__dropdown-item').click(function() {
+		$('.main-nav__list').removeClass('main-nav__list--show')
+	})
+
 	$('.main-nav__item--first').click(function() {
-		$('.main-nav__dropdown--first').toggleClass('hidden')
+		$('.main-nav__dropdown--first').toggleClass('hidden'),
+		$('.main-nav__dropdown--second').addClass('hidden'),
+		$('.main-nav__dropdown--fifth').addClass('hidden')
 	});
 
 	$('.main-nav__item--second').click(function() {
-		$('.main-nav__dropdown--second').toggleClass('hidden')
+		$('.main-nav__dropdown--second').toggleClass('hidden'),
+		$('.main-nav__dropdown--first').addClass('hidden'),
+		$('.main-nav__dropdown--fifth').addClass('hidden')
 	});
 
 	$('.main-nav__item--fifth').click(function() {
-		$('.main-nav__dropdown--fifth').toggleClass('hidden')
+		$('.main-nav__dropdown--fifth').toggleClass('hidden'),
+		$('.main-nav__dropdown--second').addClass('hidden'),
+		$('.main-nav__dropdown--first').addClass('hidden')
 	});
-
 });
