59759a68a408e24960a764ca7ccdba2cf4889a7e
Ruslan Koshevoy <ruslan.koshevoj@gmail.com>
2017-07-05 15:48:30 +0300

    updated mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 
 	$('.main-nav__menu-button').click(function() {
-		$('.main-nav__list').toggleClass('main-nav__list--show')
+		$('.main-nav__list').toggleClass('main-nav__list--show'),
+		$('.main-nav__item--first').removeClass('main-nav__item--active'),
+		$('.main-nav__item--second').removeClass('main-nav__item--active'),
+		$('.main-nav__item--fifth').removeClass('main-nav__item--active')
 	});
 
 	$('.main-nav__item--hider').click(function() {
@@ -10,24 +13,24 @@ $(document).ready(function() {
 
 	$('.main-nav__dropdown-item').click(function() {
 		$('.main-nav__list').removeClass('main-nav__list--show')
-	})
+	});
 
 	$('.main-nav__item--first').click(function() {
-		$('.main-nav__dropdown--first').toggleClass('hidden'),
-		$('.main-nav__dropdown--second').addClass('hidden'),
-		$('.main-nav__dropdown--fifth').addClass('hidden')
+		$('.main-nav__item--first').toggleClass('main-nav__item--active'),
+		$('.main-nav__item--second').removeClass('main-nav__item--active'),
+		$('.main-nav__item--fifth').removeClass('main-nav__item--active')
 	});
 
 	$('.main-nav__item--second').click(function() {
-		$('.main-nav__dropdown--second').toggleClass('hidden'),
-		$('.main-nav__dropdown--first').addClass('hidden'),
-		$('.main-nav__dropdown--fifth').addClass('hidden')
+		$('.main-nav__item--second').toggleClass('main-nav__item--active'),
+		$('.main-nav__item--first').removeClass('main-nav__item--active'),
+		$('.main-nav__item--fifth').removeClass('main-nav__item--active')
 	});
 
 	$('.main-nav__item--fifth').click(function() {
-		$('.main-nav__dropdown--fifth').toggleClass('hidden'),
-		$('.main-nav__dropdown--second').addClass('hidden'),
-		$('.main-nav__dropdown--first').addClass('hidden')
+		$('.main-nav__item--fifth').toggleClass('main-nav__item--active'),
+		$('.main-nav__item--second').removeClass('main-nav__item--active'),
+		$('.main-nav__item--first').removeClass('main-nav__item--active')
 	});
 
 	// крестик меню 
