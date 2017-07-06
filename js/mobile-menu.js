afdb0f676de57897c1b13b05d2bea40165e9e0de
morozov <morozov@quartsoft.com>
2017-07-06 14:46:44 +0300

    Рефакторинг

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,79 +1,55 @@
 $(document).ready(function() {
 
 	$('.main-nav__menu-button').click(function() {
-		$('.main-nav__list').toggleClass('main-nav__list--show'),
-		$('.main-nav__item--first').removeClass('main-nav__item--active'),
-		$('.main-nav__item--second').removeClass('main-nav__item--active'),
-		$('.main-nav__item--fifth').removeClass('main-nav__item--active')
+		$('.main-nav__list').toggleClass('main-nav__list--show');
+		$('.main-nav__item').removeClass('main-nav__item--active')
 	});
 
-	$('.main-nav__item--hider').click(function() {
+	$('.main-nav__item--hider, .main-nav__dropdown-item').click(function() {
 		$('.main-nav__list').removeClass('main-nav__list--show')
 	});
 
-	$('.main-nav__dropdown-item').click(function() {
-		$('.main-nav__list').removeClass('main-nav__list--show')
+	$('.main-nav__item').click(function () {
+		$(this).find('.main-nav__dropdown').toggleClass('main-nav__item--active');
+		$(this).nextAll().find('.main-nav__dropdown').removeClass('main-nav__item--active');
+		$(this).prevAll().find('.main-nav__dropdown').removeClass('main-nav__item--active');
 	});
 
-	$('.main-nav__item--first').click(function() {
-		$('.main-nav__item--first').toggleClass('main-nav__item--active'),
-		$('.main-nav__item--second').removeClass('main-nav__item--active'),
-		$('.main-nav__item--fifth').removeClass('main-nav__item--active')
-	});
+	// крестик меню
 
-	$('.main-nav__item--second').click(function() {
-		$('.main-nav__item--second').toggleClass('main-nav__item--active'),
-		$('.main-nav__item--first').removeClass('main-nav__item--active'),
-		$('.main-nav__item--fifth').removeClass('main-nav__item--active')
-	});
-
-	$('.main-nav__item--fifth').click(function() {
-		$('.main-nav__item--fifth').toggleClass('main-nav__item--active'),
-		$('.main-nav__item--second').removeClass('main-nav__item--active'),
-		$('.main-nav__item--first').removeClass('main-nav__item--active')
-	});
-
-	// крестик меню 
-
-	$('.main-nav__menu-button').click(function() {
-		$('#first-line').toggleClass('main-nav__menu-line--first'),
-		$('#second-line').toggleClass('main-nav__menu-line--hidden'),
-		$('#third-line').toggleClass('main-nav__menu-line--third')
-	});
-
-	$('#menu-support').click(function() {
-		$('#first-line').toggleClass('main-nav__menu-line--first'),
-		$('#second-line').toggleClass('main-nav__menu-line--hidden'),
+	$('.main-nav__menu-button, #menu-support').click(function () {
+		$('#first-line').toggleClass('main-nav__menu-line--first');
+		$('#second-line').toggleClass('main-nav__menu-line--hidden');
 		$('#third-line').toggleClass('main-nav__menu-line--third')
 	});
 
 	//fixed bar
 
-	$('.fixed-bar__hider').click(function() {
-		$('.fixed-bar').addClass('fixed-bar--collapsed'),
+	$('.fixed-bar__hider').click(function () {
+		$('.fixed-bar').addClass('fixed-bar--collapsed');
 		$('.top-games').addClass('hidden')
 	});
 
-	$('.fixed-bar__shower').click(function() {
+	$('.fixed-bar__shower').click(function () {
 		$('.fixed-bar').removeClass('fixed-bar--collapsed')
 	});
 
-	$('.fixed-bar__top-games-button').click(function() {
+	$('.fixed-bar__top-games-button').click(function () {
 		$('.top-games').toggleClass('hidden')
 	})
 
 	// крестик поля поиска
 
-	$('.main-nav__search-field').focus(function() {
+	$('.main-nav__search-field').focus(function () {
 		$('.main-nav__search-close').css('display', 'block')
 	});
 
-	$('.main-nav__search-close').click(function() {
-		$('.main-nav__search-close').css('display', 'none'),
+	$('.main-nav__search-close').click(function () {
+		$('.main-nav__search-close').css('display', 'none');
 		$('.search__filed').focusout()
 	});
 
-	$('.main-nav__search-field').focusout(function() {
+	$('.main-nav__search-field').focusout(function () {
 		$('.main-nav__search-close').css('display', 'none')
 	});
 
--- js/modals.js
+++ js/modals.js
@@ -1,60 +1,16 @@
-$(document).ready(function(){
-
-	$('.header__first-button').click(function() {
-		$('.modal-bg').css('opacity', '0.8').css('z-index', '100'),
+$(document).ready(function () {
+	$('.header__first-button, .main-nav__mobile-reg, .slide__reg-button, .registration__reg-button, #menu-support, .header__second-button, .main-nav__mobile-enter').click(function () {
+		$('.modal-bg').css({'opacity': '0.8', 'z-index': '100'}).css();
 		$('.modal-registration').css('top', '50%')
 	});
 
-	$('.main-nav__mobile-reg').click(function() {
-		$('.modal-bg').css('opacity', '0.8').css('z-index', '100'),
-		$('.modal-registration').css('top', '50%')
-	});
-
-	$('.slide__reg-button').click(function() {
-		$('.modal-bg').css('opacity', '0.8').css('z-index', '100'),
-		$('.modal-registration').css('top', '50%')
-	});
-
-	$('.registration__reg-button').click(function() {
-		$('.modal-bg').css('opacity', '0.8').css('z-index', '100'),
-		$('.modal-registration').css('top', '50%')
-	});
-
-	$('#menu-support').click(function() {
-		$('.modal-bg').css('opacity', '0.8').css('z-index', '100'),
-		$('.modal-contact').css('top', '50%')
-	});
-
-	$('.header__second-button').click(function() {
-		$('.modal-bg').css('opacity', '0.8').css('z-index', '100'),
-		$('.modal-enter').css('top', '50%')
-	});
-
-	$('.main-nav__mobile-enter').click(function() {
-		$('.modal-bg').css('opacity', '0.8').css('z-index', '100'),
-		$('.modal-enter').css('top', '50%')
-	});
-
-	$('.modal-registration__close').click(function() {
-		$('.modal-bg').css('opacity', '0').css('z-index', '-1'),
+	$('.modal-registration__close, .modal-enter__close, .modal-contact__close').click(function () {
+		$('.modal-bg').css({'opacity': '0', 'z-index': '-1'});
 		$('.modal-registration').css('top', '-1000px')
 	});
 
-	$('.modal-enter__close').click(function() {
-		$('.modal-bg').css('opacity', '0').css('z-index', '-1'),
-		$('.modal-enter').css('top', '-1000px')
+	$('.modal-bg').click(function () {
+		$('.modal-bg').css({'opacity': '0', 'z-index': '-1'});
+		$('.modal-registration, .modal-enter, .modal-contact').css('top', '-1000px')
 	});
-
-	$('.modal-contact__close').click(function() {
-		$('.modal-bg').css('opacity', '0').css('z-index', '-1'),
-		$('.modal-contact').css('top', '-1000px')
-	});
-
-	$('.modal-bg').click(function() {
-		$('.modal-bg').css('opacity', '0').css('z-index', '-1'),
-		$('.modal-registration').css('top', '-1000px'),
-		$('.modal-enter').css('top', '-1000px'),
-		$('.modal-contact').css('top', '-1000px')
-	});
-
 });
