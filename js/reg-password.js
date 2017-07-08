027c1d76012db0c3bd2bb43d876cdb31e7919cb8
Ruslan Koshevoy <ruslan.koshevoj@gmail.com>
2017-07-08 17:56:45 +0300

    replaced valyuta in registration forms

--- js/reg-password.js
+++ js/reg-password.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 
 	$('.registration-form-mini__password').focus(function() {
 		$('.registration-form-mini__password').css('margin-bottom', '76px');
-		$('.registration-form-mini__password-confirm').css({'left': '20px', 'top': '255px', 'z-index': '1', 'opacity': '1'});
+		$('.registration-form-mini__password-confirm').css({'left': '20px', 'top': '192px', 'z-index': '1', 'opacity': '1'});
 	});
 
 	$('.registration-form-mini__password').blur(function() {
 		
 		if( !$(this).val() ) {
 			$('.registration-form-mini__password').css('margin-bottom', '18px');
-			$('.registration-form-mini__password-confirm').css({'left': '20px', 'opacity': '0', 'z-index': '-1', 'top': '192px'});
+			$('.registration-form-mini__password-confirm').css({'left': '20px', 'opacity': '0', 'z-index': '-1', 'top': '130px'});
 		}
 	});
 
