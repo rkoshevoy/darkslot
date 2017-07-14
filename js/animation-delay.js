cd1d46b57607de8f25a578de34d39878fce11896
Ruslan Koshevoy <ruslan.koshevoj@mail.ru>
2017-07-14 14:51:42 +0300

    updates

--- js/animation-delay.js
+++ js/animation-delay.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-	var content = $('.content');
+	var content = $('.content'),
+	slider = $('.slider--about');
+
 	$(content).viewportChecker({
 		classToAdd: 'animated bounce',
+		offset: 300
+	});
+
+	$(slider).viewportChecker({
+		classToAdd: 'animated tada',
 		offset: 300
 	});
 });
