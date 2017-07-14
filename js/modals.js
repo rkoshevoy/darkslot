8f23ff93b8a122f56ffa2f7e2cd212af97bda4c2
Ruslan Koshevoy <ruslan.koshevoj@mail.ru>
2017-07-14 18:01:31 +0300

    modals for recovery password

--- js/modals.js
+++ js/modals.js
@@ -1,28 +1,42 @@
 $(document).ready(function () {
 	$('.header__first-button, .main-nav__mobile-reg, .slide__reg-button, .registration__reg-button, .statistics__button').click(function () {
 		$('.modal-bg').css({'opacity': '0.8', 'z-index': '100'});
-		$('.modal-registration').css('top', '50%')
+		$('.modal--registration').css('top', '50%')
 	});
 
 	$('.header__second-button, .main-nav__mobile-enter').click(function () {
 		$('.modal-bg').css({'opacity': '0.8', 'z-index': '100'});
-		$('.modal-enter').css('top', '50%')
+		$('.modal--enter').css('top', '50%')
 	});
 
-	$('#menu-support, .games__item--support').click(function () {
+	$('.modal-form__forgot-pass').click(function () {
 		$('.modal-bg').css({'opacity': '0.8', 'z-index': '100'});
-		$('.modal-contact').css('top', '50%')
+		$('.modal--recovery').css('top', '50%')
 	});
 
-	$('.modal-registration__close, .modal-enter__close, .modal-contact__close').click(function () {
+	$('#forgot-button').click(function (event) {
+		event.preventDefault();
+		$('.modal--recovery').css('top', '-1000px');
+		$('.modal--new-password').css('top', '50%')
+	});
+
+	$('#menu-support, .games__item--support, .modal-form__support').click(function () {
+		$('.modal-bg').css({'opacity': '0.8', 'z-index': '100'});
+		$('.modal--contact').css('top', '50%');
+		$('.modal--recovery').css('top', '-1000px')
+	});
+
+	$('.modal__close').click(function () {
 		$('.modal-bg').css({'opacity': '0', 'z-index': '-1'});
-		$('.modal-registration').css('top', '-1000px');
-		$('.modal-enter').css('top', '-1000px');
-		$('.modal-contact').css('top', '-1000px')
+		$('.modal--registration, .modal--enter, .modal--contact, .modal--recovery, .modal--new-password').css('top', '-1000px')
+	});
+
+	$('.modal-form__forgot-pass').click(function () {
+		$('.modal--enter').css('top', '-1000px')
 	});
 
 	$('.modal-bg').click(function () {
 		$('.modal-bg').css({'opacity': '0', 'z-index': '-1'});
-		$('.modal-registration, .modal-enter, .modal-contact').css('top', '-1000px')
+		$('.modal--registration, .modal--enter, .modal--contact, .modal--recovery, .modal--new-password').css('top', '-1000px')
 	});
 });
