a814aa0c8fb86472967dec711d0ce834c84cdd39
NadezhdaWebDeveloper <nadezhda2991@gmail.com>
2017-08-17 18:39:56 +0300

    gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
   gulp.src('dist/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer(
+      browsers: ['last 2 versions']
+    ))
     .pipe(gulp.dest('./assets/css/'))
     .pipe(browserSync.reload({stream: true}));
 });
