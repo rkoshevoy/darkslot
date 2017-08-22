9ba18ebd2800067c73cec6a47d04ad71262c3cb5
NadezhdaWebDeveloper <nadezhda2991@gmail.com>
2017-08-22 13:30:16 +0300

    niceScroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Preloader
 var preloader = document.getElementById("preloader");
 function fadeOutnojquery(el){
   el.style.opacity = 1;
@@ -12,68 +13,29 @@ function fadeOutnojquery(el){
 window.onload = function(){setTimeout(function(){fadeOutnojquery(preloader);
 },1000);};
 
-$(window).on("load",function(){
-  // $('select').styler();
+$(function() {
 
-  // $(".modal-about__scroll-content").mCustomScrollbar();
+  $('#modalPolicy, #modalRules').on('shown.bs.modal', function () {
 
-  // $('.slider').slick({
-  //   infinite: true,
-  //   autoplay: true,
-  //   autoplaySpeed: 3000,
-  //   slidesToShow: 1,
-  //   arrows: true,
-  //   responsive: [
-  //   {
-  //     breakpoint: 788,
-  //     settings: {
-  //       infinite: true,
-  //       autoplay: true,
-  //       autoplaySpeed: 3000,
-  //       slidesToShow: 1,
-  //       arrows: false
-  //     }
-  //   }
-  //   ]
-  // });
-
-  // $('.blog-slider__wrapper').slick({
-  //   infinite: true,
-  //   autoplay: true,
-  //   autoplaySpeed: 3000,
-  //   slidesToShow: 1,
-  //   arrows: true,
-  //   responsive: [
-  //   {
-  //     breakpoint: 768,
-  //     settings: {
-  //       infinite: true,
-  //       autoplay: true,
-  //       autoplaySpeed: 3000,
-  //       slidesToShow: 1,
-  //       arrows: false
-  //     }
-  //   }
-  //   ]
-  // });
-
-  // $('body').on('click', '#topGamesBtn, #gamesJackpot, #menu-games a', function(event) {
-  //   event.preventDefault();
-  //
-  //   var isHomePage = $('body').hasClass('page-home');
-  //   var anchor = 0;
-  //
-  //   if (isHomePage) {
-  //     anchor = $('#gamesPannel').offset().top - $('.header').height();
-  //     $('body, html').animate({scrollTop: anchor}, 1000);
-  //   } else {
-  //     window.location.href = '/';
-  //     setTimeout(function() {
-  //       anchor = $('#gamesPannel').offset().top - $('.header').height();
-  //       $('body, html').animate({scrollTop: anchor}, 1000);
-  //
-  //     }, 1000);
-  //   }
-  // });
+    $(".rules").getNiceScroll().remove();
+    $(".rules").niceScroll(
+      {
+        cursorcolor: "#de3137",
+        cursorborder: "1px solid #de3137",
+        cursorborderradius: 0,
+        cursoropacitymax: 0.7,
+        boxzoom: true,
+        touchbehavior: true,
+        autohidemode: "leave"
+      });
+  })
 
 });
+
+// $(document).ready(function() {
+//
+//   $("#modalPolicy").niceScroll(".rules", {cursorcolor:"#F00",cursoropacitymax:0.7,boxzoom:true,touchbehavior:true}); // First scrollable DIV
+//
+//   $(".rules").niceScroll( {cursorcolor:"#F00",cursoropacitymax:0.7,boxzoom:true,touchbehavior:true});
+//
+// });
--- js/tabs.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-	$('.switchers__switcher').click(function () {
-		$('.switchers__switcher').removeClass('switchers__switcher--active');
-		$(this).addClass('switchers__switcher--active');
-		$('.tab').removeClass('tab--active')
-		$(".tab:eq(" + $(this).index() + ")").addClass('tab--active')
-	});
-});
